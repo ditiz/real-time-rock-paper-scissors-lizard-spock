6967bbbb68353470c6241c087d0b2ba3c7938f6d
Mathieu <mathieu.25565@gmail.com>
2023-03-09 19:28:55 +0100

    docs: add control to title story

--- components/atoms/game/title.stories.tsx
+++ components/atoms/game/title.stories.tsx
@@ -1,8 +1,16 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react"
 import { GameTitleAtom } from ".."
 
 export default {
   title: "Atoms/Game/Title",
   component: GameTitleAtom,
-}
+  args: {
+    children: "Title",
+  },
+} as ComponentMeta<typeof GameTitleAtom>
 
-export const Default = () => <GameTitleAtom>Title</GameTitleAtom>
+const Template: ComponentStory<typeof GameTitleAtom> = (args) => (
+  <GameTitleAtom {...args} />
+)
+
+export const Default = Template.bind({})
