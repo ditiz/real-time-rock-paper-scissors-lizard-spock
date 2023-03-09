eedc6a867510ebbd273bdcc7cf96bcb13ab07306
Mathieu <mathieu.25565@gmail.com>
2023-03-09 19:31:15 +0100

    docs: add control to result story

--- components/atoms/game/result.stories.tsx
+++ components/atoms/game/result.stories.tsx
@@ -1,8 +1,16 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react"
 import { GameResultAtom } from ".."
 
 export default {
   title: "Atoms/Game/Result",
   component: GameResultAtom,
-}
+  args: {
+    children: "win",
+  },
+} as ComponentMeta<typeof GameResultAtom>
 
-export const Default = () => <GameResultAtom>Win</GameResultAtom>
+const Template: ComponentStory<typeof GameResultAtom> = (args) => (
+  <GameResultAtom {...args} />
+)
+
+export const Default = Template.bind({})
