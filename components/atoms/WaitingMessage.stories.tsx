23590a48342fada5c12e36c4aa290482d1d736db
Mathieu <mathieu.25565@gmail.com>
2023-03-17 23:28:52 +0100

    refactor: create atom waiting message

---
+++ components/atoms/WaitingMessage.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { WaitingMessageAtom } from "."
+
+export default {
+  title: "Atoms/Game/WaitingMessage",
+  component: WaitingMessageAtom,
+} as ComponentMeta<typeof WaitingMessageAtom>
+
+const Template: ComponentStory<typeof WaitingMessageAtom> = (args) => (
+  <WaitingMessageAtom {...args} />
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  message: "Waiting for another player",
+}
--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -1,9 +1,11 @@
 import GameResultAtom from "./game/result"
 import GameTitleAtom from "./game/title"
+import WaitingMessageAtom from "./WaitingMessage"
 // imports
 
 export {
   GameTitleAtom,
   GameResultAtom,
+  WaitingMessageAtom,
   // exports
 }
