9f425098ff4ab5012947aa29444bb8c34170af5d
Mathieu <mathieu.25565@gmail.com>
2023-03-18 21:54:15 +0100

    test(e2e): header and title

--- tests/playGame.spec.ts
+++ tests/playGame.spec.ts
@@ -5,6 +5,13 @@ const projectUrl = "http://localhost:3000/"
 test("title", async ({ page }) => {
   await page.goto(projectUrl)
 
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Rock Paper Scissor Lizard Spock/)
+})
+
+test("header", async ({ page }) => {
+  await page.goto(projectUrl)
+
   const title = await page.innerText("h1")
 
   expect(title).toBe("Rock Paper Scissors Lizard Spock")
