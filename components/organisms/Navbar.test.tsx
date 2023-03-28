50e5fac880ec43bb8733d789009d9cdf828825da
Mathieu <mathieu.25565@gmail.com>
2023-03-28 22:44:17 +0200

    test: navbar organism

--- components/organisms/Navbar.test.tsx
+++ components/organisms/Navbar.test.tsx
@@ -6,3 +6,12 @@ it("renders NavbarOrganism unchanged", () => {
   const { container } = render(<NavbarOrganism />)
   expect(container).toMatchSnapshot()
 })
+
+it("should have logo", () => {
+  // TODO: create logo and check it here
+})
+
+it("should have links", () => {
+  render(<NavbarOrganism />)
+  expect(screen.getAllByRole("link")).toHaveLength(2)
+})
